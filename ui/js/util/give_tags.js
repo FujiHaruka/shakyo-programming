5d064cb1e02ebfe276556019f7ccd1be74436916
FujiHaruka <fujiharuka@realglobe.jp>
2016-08-14 20:54:02 +0900

    HTML特殊文字対応

--- ui/js/util/give_tags.js
+++ ui/js/util/give_tags.js
@@ -1,5 +1,7 @@
 const hljs = require('highlight.js')
 const HTML = require('html-parse-stringify2')
+const Entities = require('html-entities').AllHtmlEntities
+const entities = new Entities()
 
 // TODO escape をどうするか
 /**
@@ -10,6 +12,7 @@ function giveTags (code) {
     return ''
   }
   let highlighten = hljs.highlight(code.language, code.text, true, false).value
+  console.log(highlighten)
 
   // html-parse-stringify2 ではスペースが無視されるので
   let fHighlighten = highlighten
@@ -28,6 +31,8 @@ function giveTags (code) {
   let rewrite = (content) => {
     let split = content.split('')
     let returnFlag = false // 改行後のスペースにタグ付与しないためのフラグ
+    let specialFlag = false // HTML 特殊文字
+    let specialChar = ''
     let tag = (char) => `<span id="code-char-${spanId++}">${char}</span>`
     let rewritten = split.map((char) => {
       switch (char) {
@@ -42,10 +47,33 @@ function giveTags (code) {
             keyArray.push(' ')
             return tag(char)
           }
-        default:
+        case '&': // 特殊文字開始
           returnFlag = false
-          keyArray.push(char)
-          return tag(char)
+          specialFlag = true
+          specialChar = '&'
+          return `<span id="code-char-${spanId++}">` + char
+        case ';': // 特殊文字終了
+          if (specialFlag) {
+            specialFlag = false
+            specialChar += ';'
+            keyArray.push(entities.decode(specialChar))
+            return char + '</span>'
+          } else {
+            // default
+            returnFlag = false
+            keyArray.push(char)
+            return tag(char)
+          }
+        default:
+          if (specialFlag) {
+            specialChar += char
+            return char
+          } else {
+            // default
+            returnFlag = false
+            keyArray.push(char)
+            return tag(char)
+          }
       }
     })
     return rewritten.join('')
